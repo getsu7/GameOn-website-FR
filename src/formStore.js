b5df7b2962462ecf6b3d47839efbe4d99a7d69b9
Nathan Coin <azesseza11@gmail.com>
2024-05-21 16:20:28 +0200

    feat: form validation

---
+++ src/formStore.js
@@ -0,0 +1,11 @@
+// form state
+export const formState = {
+    firstname: { value: '', valid: undefined },
+    lastname: { value: '', valid: undefined },
+    email: { value: '', valid: undefined },
+    birthdate: { value: '', valid: undefined },
+    nbTournament: { value: '', valid: undefined },
+    location: { value: '', valid: undefined },
+    termsOfUse: { value: false, valid: undefined },
+    newsletter: { value: false, valid: true }
+  };
